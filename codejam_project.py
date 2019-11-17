d360a865e8f05e4507646b2ddf5ed460597567c2
Abhimukth Chaudhuri <abhimukth.chaudhuri@mail.mcgill.ca>
2019-11-16 23:46:36 -0500

    Added method for analyzing manual processed image. Fixed angle calculation. Need to check angle output further

--- codejam_project.py
+++ codejam_project.py
@@ -1,7 +1,11 @@
 import json
 import requests
 import os.path
-from math import *
+import math
+
+def json_r(filename):
+   with open(filename) as f_in:
+       return(json.load(f_in))
 
 #Check if the JSON file with datapoint resource exists
 file_status = os.path.exists("D:\\CodeJam_2019\\Idea_Workspace\\CodeJam2019\\data.json")
@@ -20,7 +24,7 @@ if not file_status:
     print(resp_auth.text)
     # the jwt token is valid for an hour
     JWT_TOKEN = json.loads(resp_auth.text)['access_token']
-    with open('D:/CodeJam_2019/' + 'module_Another.jpg', 'rb') as f:
+    with open('D:/CodeJam_2019/' + 'thumbnail_IMG_20191116_215103.jpg', 'rb') as f:
         resp_sub_job = requests.post(JOBS_URL,
                                     headers={'Authorization': f'Bearer {JWT_TOKEN}'},
                                     files={'media': f},
@@ -70,61 +74,48 @@ with open('D:/CodeJam_2019/Idea_Workspace/CodeJam2019/data.json', mode='r', enco
 # print(xrightHip)
 # print(yrightHip)
 
+# right_arm_test_estimation = json_r("D:/CodeJam_2019/RightArm-processed/json-RightArm.json")
+# right_arm_test_estimation = json_r('D:/CodeJam_2019/thumbnail_IMG_20191116_215103-processed/json-thumbnail_IMG_20191116_215103.json')
 
-# # Gets the position of each joint to be analyzed
-# cloud_pose_estimation = json.loads(resp_get_job.text)
-# cloud_xLSHOULDER = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][26]
-# cloud_yLSHOULDER = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][27]
+# Data Analysis:
+xRSHOULDER = right_arm_test_estimation['frames'][0]['persons'][0]['pose2d']['joints'][24]
+yRSHOULDER = right_arm_test_estimation['frames'][0]['persons'][0]['pose2d']['joints'][25]
 
-# cloud_xLELBOW = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][28]
-# cloud_yLELBOW = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][29]
+xRELBOW = right_arm_test_estimation['frames'][0]['persons'][0]['pose2d']['joints'][22]
+yRELBOW = right_arm_test_estimation['frames'][0]['persons'][0]['pose2d']['joints'][23]
 
-# cloud_xLHIP = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][6]
-# cloud_yLHIP = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][7]
+xRHIP = right_arm_test_estimation['frames'][0]['persons'][0]['pose2d']['joints'][4]
+yRHIP = right_arm_test_estimation['frames'][0]['persons'][0]['pose2d']['joints'][5]
 
-# cloud_xLWRIST = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][30]
-# cloud_yLWRIST = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][31]
+xRWRIST = right_arm_test_estimation['frames'][0]['persons'][0]['pose2d']['joints'][20]
+yRWRIST = right_arm_test_estimation['frames'][0]['persons'][0]['pose2d']['joints'][21]
 
-# cloud_xRSHOULDER = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][24]
-# cloud_yRSHOULDER = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][25]
+def calculate_distance(x1, y1, x2, y2):
+    dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return dist
 
-# cloud_xRELBOW = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][22]
-# cloud_yRELBOW = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][23]
-
-# cloud_xRHIP = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][4]
-# cloud_yRHIP = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][5]
-
-# cloud_xRWRIST = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][20]
-# cloud_yRWRIST = cloud_pose_estimation['frames'][0]['persons'][0]['pose2d']['joints'][21]
-
-# def calculate_distance(x1, y1, x2, y2):
-#     dist = abs(sqrt((x2 - x1)**2 + (y2 - y1)**2))
-
-#     return dist
-
-# def calculate_angle(d1, d2, d3):
-#     angle = d1**2 = d2**2 + d3**2 - 2*a*b*cos(d1)
-
-#     return angle
-    
-# distanceL_shoulder_hip = calculate_distance(cloud_xLSHOULDER, cloud_yLSHOULDER, cloud_xLHIP, cloud_yLHIP)
-# distanceL_shoulder_elbow = calculate_distance(cloud_xLSHOULDER, cloud_yLSHOULDER, cloud_xLELBOW, cloud_yLELBOW)
-# distanceL_elbow_hip = calculate_distance(cloud_xLELBOW, cloud_yLELBOW, cloud_xLHIP, cloud_yLHIP)
-# distanceL_elbow_wrist = calculate_distance(cloud_xLELBOW, cloud_yLELBOW, cloud_xLWRIST, cloud_yLWRIST)
-# distanceL_shoulder_wrist = calculate_distance(cloud_xLSHOULDER, cloud_yLSHOULDER, cloud_xLWRIST, cloud_yLWRIST)
-
-# distanceR_shoulder_hip = calculate_distance(cloud_xRSHOULDER, cloud_yRSHOULDER, cloud_xRHIP, cloud_yRHIP)
-# distanceR_shoulder_elbow = calculate_distance(cloud_xRSHOULDER, cloud_yRSHOULDER, cloud_xRELBOW, cloud_yRELBOW)
-# distanceR_elbow_hip = calculate_distance(cloud_xRELBOW, cloud_yRELBOW, cloud_xRHIP, cloud_yRHIP)
-# distanceR_elbow_wrist = calculate_distance(cloud_xRELBOW, cloud_yRELBOW, cloud_xRWRIST, cloud_yRWRIST)
-# distanceR_shoulder_wrist = calculate_distance(cloud_xRSHOULDER, cloud_yRSHOULDER, cloud_xRWRIST, cloud_yRWRIST)
-
-# right_angle_1 = calculate_angle(distanceR_shoulder_wrist, distanceR_elbow_wrist, distanceR_shoulder_elbow)
-# right_angle_2 = calculate_angle(distanceR_elbow_hip, distanceR_shoulder_elbow, distanceR_shoulder_hip)
-# left_angle_1 = calculate_angle(distanceL_shoulder_wrist, distanceL_elbow_wrist, distanceL_shoulder_elbow)
-# left_angle_2 = calculate_angle(distanceL_elbow_hip, distanceL_shoulder_elbow, distanceL_shoulder_hip)
+def calculate_angle(d1, d2, d3):
+    angle = math.acos((d2**2 + d3**2 - d1**2)/(2*d2*d3))
+    return math.degrees(angle)
 
 
-# # Analyse the 3 pictures (up, middle, down)
-# for i in range (3):
-    
+distanceR_shoulder_hip = calculate_distance(xRSHOULDER, yRSHOULDER, xRHIP, yRHIP)
+distanceR_shoulder_elbow = calculate_distance(xRSHOULDER, yRSHOULDER, xRELBOW, yRELBOW)
+distanceR_elbow_hip = calculate_distance(xRELBOW, yRELBOW, xRHIP, yRHIP)
+distanceR_elbow_wrist = calculate_distance(xRELBOW, yRELBOW, xRWRIST, yRWRIST)
+distanceR_shoulder_wrist = calculate_distance(xRSHOULDER, yRSHOULDER, xRWRIST, yRWRIST)
+
+print(distanceR_shoulder_wrist, distanceR_elbow_wrist, distanceR_shoulder_elbow)
+print(distanceR_elbow_hip, distanceR_shoulder_elbow, distanceR_shoulder_hip)
+
+# Elbow bend angle 
+right_angle_1 = calculate_angle(distanceR_shoulder_wrist, distanceR_elbow_wrist, distanceR_shoulder_elbow)
+print(right_angle_1)
+# Arm-pit angle 
+right_angle_2 = calculate_angle(distanceR_elbow_hip, distanceR_shoulder_elbow, distanceR_shoulder_hip)
+print(right_angle_2)
+
+if right_angle_1 < 80 or right_angle_2 < 80:
+    print("You have a bad position.")
+else:
+    print("Your position is good!")
